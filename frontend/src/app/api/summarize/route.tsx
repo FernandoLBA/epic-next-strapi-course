1bc6d8786ae47bd60cb0152fc9150bed63b3a8f5
fernandolba <fbarrios.pyc@gmail.com>
2024-05-08 15:45:47 -0500

    youtube transcription service implemented

--- frontend/src/app/api/summarize/route.tsx
+++ frontend/src/app/api/summarize/route.tsx
@@ -1,34 +1,122 @@
 import { NextRequest } from "next/server";
 
+import { getAuthToken } from "@/data/services/get-token";
+import { getUserMeLoader } from "@/data/services/get-user-me-loader";
+import { fetchTranscript } from "@/lib/youtube-transcript";
+
+/**
+ * 
+ * @param id 
+ * @returns 
+ */
+async function getTranscript(id: string) {
+    try {
+      return await fetchTranscript(id);
+    } catch (error) {
+      if (error instanceof Error)
+        return new Response(JSON.stringify({ error: error.message }));
+      return new Response(
+        JSON.stringify({ error: "Failed to generate summary." })
+      );
+    }
+  }
+
+/**
+ * Recibe un array de data: any[]
+ * Concatena todas las cadenas de transcripciones
+ * y las retorna en un objeto:
+ * {
+ *  data: data, // es el array como vino
+ *  text: text.trim() // aquí estaría todas las transcripciones juntas en un solo texto
+ * }
+ * @param data
+ * @returns
+ */
+function transformData(data: any[]) {
+  let text = "";
+
+  // * Itera la data y va concatenando los textos de la data en uno solo
+  data.forEach((item) => {
+    text += item.text + " ";
+  });
+
+  return {
+    data: data,
+    text: text.trim(),
+  };
+}
+
 /**
  * Este componente recibe una petición (Request) POST.
- * 
+ *
  * Se encarga de manejar las respuestas de una petición específica,
  * en este caso de tipo POST, bien sean exitosas o erróneas.
- * 
+ *
  * Este route handler maneja peticiones de forma customizada, y todas las
  * peticiones a /api/summarize que sean de tipo POST pasarán por aquí
  * de maner automática.
- * 
- * @param req 
- * @returns 
+ * @param req
+ * @returns
  */
 export async function POST(req: NextRequest) {
-    console.log("FROM OUR ROUTE HANDLER:", req.body);
-    try {
-        // * Retorna una nueva respuesta con la data, sin error y status 200
-        return new Response(
-            JSON.stringify({ data: "return from our handler", error: null }),
-            {
-                status: 200,
-            }
-        );
-    } catch (error) {
-        console.error("Error processing request:", error);
+  // * Obtiene la información del usuario y el token
+  const user = await getUserMeLoader();
+  const token = await getAuthToken();
 
-        // * SI hay error retorna una nueva respuesta con el error
-        if (error instanceof Error)
-            return new Response(JSON.stringify({ error: error }));
-        return new Response(JSON.stringify({ error: "Unknown error" }));
-    }
+  // * Si el usuario no está logueado
+  if (!user.ok || !token) {
+    return new Response(
+      JSON.stringify({ data: null, error: "Not authenticated" }),
+      { status: 401 }
+    );
+  }
+
+  //   * Revisa los créditos del usuario
+  if (user.data.credits < 1) {
+    return new Response(
+      JSON.stringify({
+        data: null,
+        error: "Insufficient credits",
+      }),
+      { status: 402 }
+    );
+  }
+
+  // * Convierte el req en json
+  const body = await req.json();
+  const { videoId } = body;
+
+  // * esto es un tipado genérico que obtendrá la respuesta de tipo fetchTranscript
+  let transcript: Awaited<ReturnType<typeof getTranscript>>;
+
+  try {
+    transcript = await getTranscript(videoId);
+  } catch (error) {
+    console.error("Error processing request:", error);
+
+    // * SI hay error retorna una nueva respuesta con el error
+    if (error instanceof Error)
+      return new Response(JSON.stringify({ error: error.message }));
+    return new Response(JSON.stringify({ error: "Unknown error" }));
+  }
+
+  // * Transforma la data, recibiendo la data como está y una propiedad text con toda la transcripción junta
+  const transformedData = transformData(transcript);
+
+  try {
+    // * Retorna una nueva respuesta con la data, sin error y status 200
+    return new Response(
+      JSON.stringify({ data: "return from our handler", error: null }),
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.error("Error processing request:", error);
+
+    // * SI hay error retorna una nueva respuesta con el error
+    if (error instanceof Error)
+      return new Response(JSON.stringify({ error }));
+    return new Response(JSON.stringify({ error: "Unknown error" }));
+  }
 }
--- frontend/src/components/forms/SummaryForm.tsx
+++ frontend/src/components/forms/SummaryForm.tsx
@@ -3,86 +3,118 @@
 import { useState } from "react";
 import { toast } from "sonner";
 
-import { cn } from "@/lib/utils";
-import { Input } from "@/components/ui/input";
 import { SubmitButton } from "@/components/custom/SubmitButton";
+import { Input } from "@/components/ui/input";
 import { generateSummaryService } from "@/data/services/summary-service";
+import { cn, extractYouTubeID } from "@/lib/utils";
 
 interface StrapiErrorsProps {
-    message: string | null;
-    name: string;
+  message: string | null;
+  name: string;
 }
 
 const INITIAL_STATE = {
-    message: null,
-    name: "",
+  message: null,
+  name: "",
 };
 
 export function SummaryForm() {
-    // * Controla si está cargando o no
-    const [loading, setLoading] = useState(false);
-    // * Controla los erreos de strapi
-    const [error, setError] = useState<StrapiErrorsProps>(INITIAL_STATE);
-    // * establece el valor del input
-    const [value, setValue] = useState<string>("");
+  // * Controla si está cargando o no
+  const [loading, setLoading] = useState(false);
+  // * Controla los erreos de strapi
+  const [error, setError] = useState<StrapiErrorsProps>(INITIAL_STATE);
+  // * establece el valor del input
+  const [value, setValue] = useState<string>("");
 
-    /**
-     * Recibe el evento del form y controla el envío del formulario
-     * @param event 
-     */
-    async function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
-        event.preventDefault();
-        setLoading(true);
+  /**
+   * Recibe el evento del form y controla el envío del formulario
+   * @param event
+   */
+  async function handleFormSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setLoading(true);
 
-        // * Crea una instancia de formData y le envía el currentTarget del evento
-        const formData = new FormData(event.currentTarget);
-        // * Extrae el videoId como string
-        const videoId = formData.get("videoId") as string;
+    // * Crea una instancia de formData y le envía el currentTarget del evento
+    const formData = new FormData(event.currentTarget);
+    // * Extrae el videoId como string
+    const videoId = formData.get("videoId") as string;
+    // * Extrae el indetificador del video de youtube
+    const processedVideoId = extractYouTubeID(videoId);
 
-        // * consume el servicio generateSummaryService y le manda el videoId
-        const summaryReponseData = await generateSummaryService(videoId);
-        console.log("🚀 ~ handleFormSubmit ~ summaryReponseData:", summaryReponseData)
+    // * Si processedVideoId es null emite un error
+    if (!processedVideoId) {
+      toast.error("Invalid YouTube Video ID");
+      setLoading(false);
+      setValue("");
 
-        toast.success("Summary Created");
-        setLoading(false);
+      setError({
+        ...INITIAL_STATE,
+        message: "Invalid YouTube Video ID",
+        name: "Invalid Id",
+      });
+
+      return;
     }
 
-    function clearError() {
-        setError(INITIAL_STATE);
-        if (error.message) setValue("");
+    // * consume el servicio generateSummaryService y le manda el videoId
+    const summaryReponseData = await generateSummaryService(videoId);
+
+    // * Si hay un error en la respuesta
+    if (summaryReponseData.error) {
+      setValue("");
+
+      toast.error(summaryReponseData.error);
+
+      setError({
+        ...INITIAL_STATE,
+        message: summaryReponseData.error,
+        name: "Summary Error",
+      });
+
+      setLoading(false);
+      return;
     }
 
-    const errorStyles = error.message
-        ? "outline-1 outline outline-red-500 placeholder:text-red-700"
-        : "";
+    toast.success("Summary Created");
+    setLoading(false);
+  }
 
-    return (
-        <div className="w-full max-w-[960px]">
-            <form
-                onSubmit={handleFormSubmit}
-                className="flex gap-2 items-center justify-center"
-            >
-                <Input
-                    name="videoId"
-                    placeholder={
-                        error.message ? error.message : "Youtube Video ID or URL"
-                    }
-                    value={value}
-                    onChange={(e) => setValue(e.target.value)}
-                    onMouseDown={clearError}
-                    className={cn(
-                        "w-full focus:text-black focus-visible:ring-pink-500",
-                        errorStyles
-                    )}
-                    required
-                />
+  function clearError() {
+    setError(INITIAL_STATE);
+    if (error.message) setValue("");
+  }
 
-                <SubmitButton
-                    text="Create Summary"
-                    loadingText="Creating Summary"
-                    loading={loading}
-                />
-            </form>
-        </div>
-    );
+  const errorStyles = error.message
+    ? "outline-1 outline outline-red-500 placeholder:text-red-700"
+    : "";
+
+  return (
+    <div className="w-full max-w-[960px]">
+      <form
+        onSubmit={handleFormSubmit}
+        className="flex gap-2 items-center justify-center"
+      >
+        <Input
+          name="videoId"
+          placeholder={
+            error.message ? error.message : "Youtube Video ID or URL"
+          }
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onMouseDown={clearError}
+          className={cn(
+            "w-full focus:text-black focus-visible:ring-pink-500",
+            errorStyles
+          )}
+          required
+        />
+
+        <SubmitButton
+          text="Create Summary"
+          loadingText="Creating Summary"
+          loading={loading}
+        />
+      </form>
+    </div>
+  );
 }
