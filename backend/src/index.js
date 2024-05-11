567d88c6101df7866a72909c63227f883f7ce6f6
fernandolba <fbarrios.pyc@gmail.com>
2024-05-11 03:02:15 -0500

    middlware user-can-update who protects the update method in users-permissions was created

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,6 +14,8 @@ module.exports = {
 
     // Guardamos el middleware user-find-many que queremos usar
     const isUserOwnerMiddleware = "global::user-find-many";
+    // Agregamos otro
+    const isUserCanUpdateMiddleware = "global::user-can-update";
 
     // Mapea las rutas y busca el indice de la ruta dónde queremos aplicar el middleware
     const findUser = userRoutes.findIndex(
@@ -21,13 +23,18 @@ module.exports = {
       (route) => route.handler === "user.find" && route.method === "GET"
     );
 
+    const updateUser = userRoutes.findIndex(
+      // Recibe una ruta, y si coincide con el nombre y el método devuelve su posición
+      (route) => route.handler === "user.update" && route.method === "PUT"
+    );
+
     /**
-     * Esta función setea los middlewares y las policies si existen, 
+     * Esta función setea los middlewares y las policies si existen,
      * sino hay nada, agrega un array vacío.
-     * 
+     *
      * Recibe el array de rutas y la posición de la ruta que usaremos
-     * @param {*} routes 
-     * @param {*} index 
+     * @param {*} routes
+     * @param {*} index
      */
     function initializeRoute(routes, index) {
       routes[index].config.middlewares = routes[index].config.middlewares || [];
@@ -35,12 +42,21 @@ module.exports = {
     }
 
     // Si encuentra el index
-    if(findUser){
+    if (findUser) {
       // Setea los middlewares y policies a la ruta
       initializeRoute(userRoutes, findUser);
       // Agrega el middleware que usaremos a la ruta
-      userRoutes[findUser].config.middlewares.push(isUserOwnerMiddleware)
+      userRoutes[findUser].config.middlewares.push(isUserOwnerMiddleware);
     }
+
+    if (updateUser) {
+      // Setea los middlewares y policies a la ruta
+      initializeRoute(userRoutes, updateUser);
+      // Agrega el middleware que usaremos a la ruta
+      userRoutes[updateUser].config.middlewares.push(isUserCanUpdateMiddleware);
+    }
+
+    console.log(userRoutes[updateUser]);
   },
 
   /**
